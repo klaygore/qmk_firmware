f031a1c3d5205c40ac383f5e36a4e6c6738ee5f7
clay <seetheclay@live.com>
2020-04-14 19:38:05 -0500

    added tapping_term_per_key to config.h to support space modifiers.

--- keyboards/boardwalk/config.h
+++ keyboards/boardwalk/config.h
@@ -87,4 +87,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* set tap time between multiple taps using tap dance */
 /* set tap count to 3 */
 #define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
 #define TAPPING_TOGGLE 3
