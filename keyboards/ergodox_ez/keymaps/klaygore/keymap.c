56ada8c0c6ccaa2cbbe69039b136f1e304505e18
clay <seetheclay@live.com>
2020-06-02 12:11:50 -0500

    added modifier keys to base layer for admin use. press modifiers with KC_NO to be paired with whatever key is needed for quick mod shortcuts.

--- keyboards/ergodox_ez/keymaps/klaygore/keymap.c
+++ keyboards/ergodox_ez/keymaps/klaygore/keymap.c
@@ -44,15 +44,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC   |   1  |   2  |   3  |   4  |   5  |MEH(F)|           | `~   |   6  |   7  |   8  |   9  |   0  |  DEL   |
+ * |  ESC   |   1  |   2  |   3  |   4  |   5  |CTLGUI|           | `~   |   6  |   7  |   8  |   9  |   0  |  DEL   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | SFTTAB |   Q  |   W  |   E  |   R  |   T  | CAPS |           | NUM  |   Y  |   U  |   I  |   O  |   P  |  SFT \ |
  * |--------+------+------+------+------+------| LOCK |           | LOCK |------+------+------+------+------+--------|
  * | ENTER  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |  '"    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCTRL  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |  RCTRL |
+ * |--------+------+------+------+------+------| CTL  |           | MEH  |------+------+------+------+------+--------|
+ * | LCTRL  |   Z  |   X  |   C  |   V  |   B  | SFT  |           |      |   N  |   M  |   ,  |   .  |  /   |  RCTRL |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      | FIND | Alt  | LGUI |BSPACE|                                       |SPACE |      | RGUI |SEARCH|      |
+ *   |SFTGUI| FIND | Alt  | LGUI |BSPACE|                                       |SPACE |CTLALT| RGUI |SEARCH|ALTSFT|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,------------.
  *                                        | HOME | END  |       | LEFT |RIGHT|
@@ -64,20 +64,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_ESC,         KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    MEH(KC_F),
+  KC_ESC,         KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    LCTL(LGUI(KC_NO)),
   LSFT_T(KC_TAB), KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,    KC_CAPS,
   KC_ENT,         KC_A,          KC_S,    KC_D,    KC_F,    KC_G,
-  KC_LCTL,        KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
-  KC_NO,          LCTL(KC_F),    KC_LALT, KC_LGUI, LT(NUML,KC_BSPC),
+  KC_LCTL,        KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,    C(S(KC_NO)),
+  SGUI(KC_NO),    LCTL(KC_F),    KC_LALT, KC_LGUI, LT(NUML,KC_BSPC),
                                                                       KC_HOME,  KC_END,
                                                                                 KC_PGUP,
                                                             TT(SYMB), TT(MDIA), KC_PGDN,
   // right hand
-  KC_GRV,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,           KC_DEL,
-  KC_NLCK,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           RSFT_T(KC_BSLS),
-  KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_NO,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-  LT(SPMOD,KC_SPC), KC_NO,   KC_RGUI, KC_WSCH, KC_NO,
+  KC_GRV,           KC_6,        KC_7,    KC_8,    KC_9,    KC_0,           KC_DEL,
+  KC_NLCK,          KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,           RSFT_T(KC_BSLS),
+  KC_H,             KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  MEH(KC_NO),       KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+  LT(SPMOD,KC_SPC), LCA(KC_NO),  KC_RGUI, KC_WSCH, A(S(KC_NO)),
   KC_LEFT, KC_RIGHT,
   KC_UP,
   KC_DOWN, KC_ENT, TT(SYMB2)
