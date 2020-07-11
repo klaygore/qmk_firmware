a76128122d5e2dc8e4765bf952a147a9481a94fd
klaygore <seetheclay@live.com>
2020-07-10 20:23:18 -0500

    moved run_cmd to space modifier layer and changed that key on the base layer to lgui+t for opening terminal.

--- keyboards/boardwalk/keymaps/klaygore/keymap.c
+++ keyboards/boardwalk/keymaps/klaygore/keymap.c
@@ -81,16 +81,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * |  LCTRL |   Z    |   X    |   C    |   V    |   B    |  PGUP  |   N    |   M    |   ,    |   .    |   /    |  UP    | RCTRL  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | SEARCH |  FIND  |  ALT   |  LGUI  | BACKSP | LOWER  |  PGDN  | RAISE  | SPACE  | RUN_CMD|  RGUI  |  LEFT  |  DOWN  | RIGHT  |
+ * | SEARCH |  FIND  |  ALT   |  LGUI  | BACKSP | LOWER  |  PGDN  | RAISE  | SPACE  |TERMINAL|  LGUI  |  LEFT  |  DOWN  | RIGHT  |
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_MAIN] = LAYOUT_ortho_5x14(
-  TD(ESC_CAPS),   KC_1,       KC_2,    KC_3,    KC_4,          KC_5,    KC_HOME,  KC_6,  KC_7,          KC_8,      KC_9,    KC_0,    KC_GRV,  KC_DEL, \
-  LSFT_T(KC_TAB), KC_Q,       KC_W,    KC_E,    KC_R,          KC_T,    KC_END,   KC_Y,  KC_U,          KC_I,      KC_O,    KC_P,    KC_BSLS, KC_RSFT, \
-  KC_ENT,         KC_A,       KC_S,    KC_D,    KC_F,          KC_G,    KC_MUTE,  KC_H,  KC_J,          KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
-  KC_LCTL,        KC_Z,       KC_X,    KC_C,    KC_V,          KC_B,    KC_PGUP,  KC_N,  KC_M,          KC_COMM,   KC_DOT,  KC_SLSH, KC_UP,   KC_RCTL, \
-  KC_WSCH,        LCTL(KC_F), KC_LALT, KC_LGUI, LT(4,KC_BSPC), TT(2),   KC_PGDN,  TT(3), LT(5,KC_SPC),  RUN_CMD,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT\
+  TD(ESC_CAPS),   KC_1,       KC_2,    KC_3,    KC_4,          KC_5,    KC_HOME,  KC_6,  KC_7,          KC_8,       KC_9,    KC_0,    KC_GRV,  KC_DEL, \
+  LSFT_T(KC_TAB), KC_Q,       KC_W,    KC_E,    KC_R,          KC_T,    KC_END,   KC_Y,  KC_U,          KC_I,       KC_O,    KC_P,    KC_BSLS, KC_RSFT, \
+  KC_ENT,         KC_A,       KC_S,    KC_D,    KC_F,          KC_G,    KC_MUTE,  KC_H,  KC_J,          KC_K,       KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
+  KC_LCTL,        KC_Z,       KC_X,    KC_C,    KC_V,          KC_B,    KC_PGUP,  KC_N,  KC_M,          KC_COMM,    KC_DOT,  KC_SLSH, KC_UP,   KC_RCTL, \
+  KC_WSCH,        LCTL(KC_F), KC_LALT, KC_LGUI, LT(4,KC_BSPC), TT(2),   KC_PGDN,  TT(3), LT(5,KC_SPC),  LGUI(KC_T), KC_LGUI, KC_LEFT, KC_DOWN, KC_RGHT\
  ),
 
 /* custom gaming - layer 1
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+-----------------+--------+--------+--------|
- * |        |        |        |        |  DEL   |        |        |        |        |        |        |        |        |        |
+ * |        | RUN_CMD|        |        |  DEL   |        |        |        |        |        |        |        |        |        |
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -205,7 +205,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, RUN_CMD, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______ \
  ),
  
  /* SYSTEM - layer 6
