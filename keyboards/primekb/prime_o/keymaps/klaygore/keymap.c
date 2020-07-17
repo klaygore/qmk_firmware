5d1c153f5ca7da8bb4b257728543af5a29ea210e
klaygore <seetheclay@live.com>
2020-07-16 21:47:15 -0500

    removed no longer used macros from keys on wow layer

--- keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
+++ keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
@@ -288,11 +288,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |      | ' "  |  4   |  5   |  6   |  -   |
  * | SHIFT|      |      |      |      |      |      |      |      |      |  ;:  |SHIFT | LEFT |      | RIGHT|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |MOUSEB|  1   |  2   |  3   | QUAD |
- * | Ctrl |      |      |      |      |      |      |      |      |      |      | 1/2  | END  | DOWN |PGDN  |  +   |
+ * |      |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |MOUSEB|  1   |  2   |  3   |  +   |
+ * | Ctrl |      |      |      |      |      |      |      |      |      |      | 1/2  | END  | DOWN |PGDN  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | ` ~  | TD   |      |      |BSpace|LOWER |RAISE |Space | RCTL | CALT | Left | Down | Right|  0   |  .   | ENTER|
- * |      | TRI_P|  Alt | GUI  |      |      |      |      |      |  F   |      |      |      |INSERT| DEL  |  =   |
+ * | ` ~  |      |      |      |BSpace|LOWER |RAISE |Space | RCTL |      | Left | Down | Right|  0   |  .   |  =   |
+ * |      |      |  Alt | GUI  |      |      |      |      |      |      |      |      |      |INSERT| DEL  |      |
  * `---------------------------------------------------------------------------------------------------------------'
  */
 
@@ -300,8 +300,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD(CAP_ESC),    KC_F1,      KC_F2,    KC_F3,    KC_F4,   KC_F5,     KC_F6,     KC_F7,   KC_F8,    KC_F9,          KC_F10,    KC_DEL,          KC_NLCK,    KC_MPLY,   TD(NXT_PRV), KC_PSLS,
     LSFT_T(KC_TAB), KC_Q,       KC_W,     KC_E,     KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,     KC_O,           KC_P,      RSFT_T(KC_BSLS), KC_P7,      KC_P8,     KC_P9,       KC_PAST,
     KC_ENT,         KC_A,       KC_S,     KC_D,     KC_F,    KC_G,      KC_H,      KC_J,    KC_K,     KC_L,           KC_SCLN,   KC_QUOT,         KC_P4,      KC_P5,     KC_P6,       KC_PMNS,
-    KC_LCTL,  	    KC_Z,       KC_X,     KC_C,     KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM,  KC_DOT,         KC_SLSH,   TD(DBL_MOUS),    KC_P1,      KC_P2,     KC_P3,       TD(QUAD_PLUS),
-    KC_GRV,         TD(TRI_P),  KC_LALT,  KC_LGUI,  KC_BSPC, TT(LOWER), TT(RAISE), KC_SPC,  KC_RCTL,  OSL(WOWMACROS), KC_LEFT,   KC_DOWN,         KC_RIGHT,   KC_P0,     KC_PDOT,     TD(ENT_EQL)
+    KC_LCTL,  	    KC_Z,       KC_X,     KC_C,     KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM,  KC_DOT,         KC_SLSH,   TD(DBL_MOUS),    KC_P1,      KC_P2,     KC_P3,       KC_PPLS,
+    KC_GRV,         KC_NO,      KC_LALT,  KC_LGUI,  KC_BSPC, TT(LOWER), TT(RAISE), KC_SPC,  KC_RCTL,  KC_NO,          KC_LEFT,   KC_DOWN,         KC_RIGHT,   KC_P0,     KC_PDOT,     KC_PEQL
   ),
 
 /* [3] WOW MACROS
