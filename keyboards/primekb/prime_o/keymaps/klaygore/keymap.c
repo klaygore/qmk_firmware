3d13d64eccb010790b3c74e47d00da45e8b836d9
klaygore <seetheclay@live.com>
2020-08-29 11:04:58 -0500

    added gui keybind back to right gui key for wow layer

--- keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
+++ keyboards/primekb/prime_o/keymaps/klaygore/keymap.c
@@ -301,7 +301,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LSFT_T(KC_TAB), KC_Q,          KC_W,     KC_E,     KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,     KC_O,           KC_P,      RSFT_T(KC_BSLS), KC_P7,      KC_P8,     KC_P9,       KC_PAST,
     KC_ENT,         KC_A,          KC_S,     KC_D,     KC_F,    KC_G,      KC_H,      KC_J,    KC_K,     KC_L,           KC_SCLN,   KC_QUOT,         KC_P4,      KC_P5,     KC_P6,       KC_PMNS,
     KC_LCTL,  	    KC_Z,          KC_X,     KC_C,     KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM,  KC_DOT,         KC_SLSH,   TD(DBL_MOUS),    KC_P1,      KC_P2,     KC_P3,       KC_PPLS,
-    KC_GRV,         MO(WOWMACROS), KC_LALT,  KC_LGUI,  KC_BSPC, TT(LOWER), TT(RAISE), KC_SPC,  KC_RCTL,  KC_NO,          KC_LEFT,   KC_DOWN,         KC_RIGHT,   KC_P0,     KC_PDOT,     KC_PEQL
+    KC_GRV,         MO(WOWMACROS), KC_LALT,  KC_LGUI,  KC_BSPC, TT(LOWER), TT(RAISE), KC_SPC,  KC_RCTL,  KC_LGUI,          KC_LEFT,   KC_DOWN,         KC_RIGHT,   KC_P0,     KC_PDOT,     KC_PEQL
   ),
 
 /* [3] WOW MACROS
