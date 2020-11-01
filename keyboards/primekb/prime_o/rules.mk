c4d4aef7f040531845dc5d0ad8547431778ac9c8
klaygore <seetheclay@live.com>
2020-10-31 23:55:08 -0500

    added a key lock and mouse button 2 to wow layer for albion online

--- keyboards/primekb/prime_o/rules.mk
+++ keyboards/primekb/prime_o/rules.mk
@@ -32,3 +32,4 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
 TAP_DANCE_ENABLE=yes		# Enable the tap dance options
+KEY_LOCK_ENABLE=yes		# Enable the key lock function
